a9463901f136e2f7a48a3d05cf6dcd51caa4e889
srii03 <162790549+srii03@users.noreply.github.com>
2024-04-30 03:17:31 +1000

    Update test.js

--- test.js
+++ test.js
@@ -1,45 +1,48 @@
+// Import required modules for testing
+const expect = require("chai").expect;
+const request = require("request");
 
-const { expect } = require('chai');
-const request = require('request');
-
+// Test cases for '/addTwoNumbers' route
 describe("Add Two Numbers API", function() {
-  var url = "http://localhost:8080/addTwoNumbers/3/5"; L
-
-  
-  it("should return status 200", function(done) {
-    request(url, function(error, response, body) {
-      expect(response.statusCode).to.equal(200);
-      done();
+    const url = "http://localhost:8080/addTwoNumbers/3/5";
+    
+    it("should return status 200", function(done) {
+        request(url, function(error, response, body) {
+            expect(response.statusCode).to.equal(200);
+            done();
+        });
     });
-  });
 
-  it("should return the correct result", function(done) {
-    request(url, function(error, response, body) {
-      body = JSON.parse(body);
-      expect(body.result).to.equal(8); 
-      done();
+    it("should have statusCode key in body with value 200", function(done) {
+        request(url, function(error, response, body) {
+            const responseBody = JSON.parse(body);
+            expect(responseBody.statusCode).to.equal(200);
+            done();
+        });
     });
-  });
+
+    it("should return a number as result", function(done) {
+        request(url, function(error, response, body) {
+            const responseBody = JSON.parse(body);
+            expect(responseBody.result).to.be.a('number');
+            done();
+        });
+    });
+
+    it("should return result equal to 8", function(done) {
+        request(url, function(error, response, body) {
+            const responseBody = JSON.parse(body);
+            expect(responseBody.result).to.equal(8);
+            done();
+        });
+    });
+
+    it("should not return result equal to 15", function(done) {
+        request(url, function(error, response, body) {
+            const responseBody = JSON.parse(body);
+            expect(responseBody.result).to.not.equal(15);
+            done();
+        });
+    });
 });
 
-
-describe("Add Two Strings API", function() {
-  var url = "http://localhost:8080/addTwoNumbers/a/b"; 
-
-  // Test case to check if the API returns status 400
-  it("should return status 400", function(done) {
-    request(url, function(error, response, body) {
-      expect(response.statusCode).to.equal(400);
-      done();
-    });
-  });
-
-  
-  it("should return null as result", function(done) {
-    request(url, function(error, response, body) {
-      body = JSON.parse(body);
-      expect(body.result).to.be.null; 
-      done();
-    });
-  });
-});
